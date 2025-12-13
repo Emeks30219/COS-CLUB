238295842382fe3e1fc6bc982692d026372456ff
promiseaborishade05-web <promiseaborishade05@gmail.com>
2025-12-13 15:53:26 +0100

    Update DICT.py

--- DICT.py
+++ DICT.py
@@ -1,4 +1,27 @@
 #  English to African/Nigerian language dictionary translator
 # Each group member contributed one dictionary of 20 words.
+yoruba = {
+   "water" : "omi",
+     "good morning" : "E kaaaaro",
+      "good evening" : " E kaale",
+       "come" : "wa",
+       "take" : "gba",
+       "food" : "ounje",
+       "child" : "Omo",
+       "friend" : "ore",
+       "mother" : "iya",
+       "father" : "baba",
+       "news" : "iroyin",
+       "book" : "iwe",
+       "knowledge" : "imo",
+       "city" : "ilu",
+       "time" : "igba",
+       "family" : "idile",
+       "head" : "ori",
+       "love" : "ife",
+       "waist" : "ibadi",
+       "road" : "ono",
+}
+
 
 
